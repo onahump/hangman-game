5add5587369922f27c9228d4e31fa0e663a95ecb
Nahum Okysoft <nahum.poar@gmail.com>
2019-05-27 17:58:12 -0500

    Adding states to our game

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -1,3 +1,13 @@
+
+var STATE_GAME_NONE                 = 0;
+var STATE_GAME_LOADING              = 1;
+var STATE_GAME_MAIN_MENU            = 2;
+var STATE_GAME_PLAYING              = 4;
+var STATE_GAME_GAME_OVER            = 5;
+var STATE_GAME_WIN                  = 6;
+
+var stateGame = STATE_GAME_NONE; //estado inicial
+
 GamePlayManager = {   //ObjetoGamePlayManager
     init: function(){
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; //Haciendo nuestro juego totalmente responsivo
@@ -5,19 +15,50 @@ GamePlayManager = {   //ObjetoGamePlayManager
         game.scale.pageAlignVertically = true; // Centrando nuestro juego verticalmente
     },
     preload: function(){
+        stateGame = STATE_GAME_LOADING;
         game.load.image('background', 'assets/img/background-image.png');
         game.load.image('button-play', 'assets/img/button-play2.png');
+        game.load.image('title', 'assets/img/title.png');
+        game.load.image('hangman', 'assets/img/complete_hangman.png');
     },
     create: function(){
         game.add.sprite(0, 0, 'background');
-        game.add.sprite(340, 470, 'button-play');
+        this.title = game.add.sprite(game.width/2, 100, 'title');
+        this.hangmanComplete = game.add.sprite(game.width/2, 350, 'hangman');
+        this.buttonPlay = game.add.button(game.width/2, 500, 'button-play', this.startGame, this); //new Button(posicion x , posicion x, llave , funcion a llamar , callback Context GAMEPlayManager (this), imagen antes de pasar el raton, imagen al pasar el raton, imagen a cargar antes de dar click, imagen al cagar al dar click)
+    },
+    startGame: function(){
+        stateGame = STATE_GAME_PLAYING;
     },
     update: function(){
+        switch(stateGame){ //Maquina de estados
+            case STATE_GAME_NONE:
+                break;
 
+            case STATE_GAME_LOADING:
+                break;
+            
+            case STATE_GAME_MAIN_MENU:
+
+                break;
+            
+            case STATE_GAME_PLAYING:
+                    this.title.visible = false;
+                    this.hangmanComplete.visible = false;
+                    this.buttonPlay.visible = false;
+                break;
+
+            case STATE_GAME_GAME_OVER:
+                    console.log("Game Over")
+                break;
+
+            case STATE_GAME_WIN:
+                break;
+        }
     }
 }
 
-var game = new Phaser.Game(1136,640, Phaser.CANVAS); //instanciamos juego en facer con sus especificaciones
+var game = new Phaser.Game(1136,640, Phaser.AUTO); //instanciamos juego en facer con sus especificaciones
 
 game.state.add('gameplay',GamePlayManager); //Agregamos un estado que le vamos a llamar gameplay y se le asigna el objeto GamePlayMañager
 game.state.start('gameplay'); //Llama a cada uno de los metodos
